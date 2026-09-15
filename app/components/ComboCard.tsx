559f3551cea6ac7c8a6f121f51e3e91132957526
Al1k04 <alikprog04@gmail.com>
2026-09-15 21:19:54 +0400

    fix: align combo images in a row

--- app/components/ComboCard.tsx
+++ app/components/ComboCard.tsx
@@ -1,4 +1,5 @@
 import { Combo } from "@/types/combo";
+import { FaFacebook } from "react-icons/fa";
 
 type ComboCardProps = {
   combo: Combo;
@@ -7,6 +8,7 @@ type ComboCardProps = {
 export default function ComboCard({ combo }: ComboCardProps) {
   return (
     <li className="flex flex-col gap-1 bg-neutral-900 border border-neutral-800 p-3 rounded-lg hover:border-emerald-500 transition-colors ease-in-out">
+      {/* Фотографии в один ряд */}
       <div className="flex justify-center gap-2 mb-4">
         {combo.images.map((image, index) => (
           <img
@@ -18,13 +20,16 @@ export default function ComboCard({ combo }: ComboCardProps) {
         ))}
       </div>
 
+      {/* Название */}
       <div className="flex gap-2 mb-4">
         <span>{combo.id}.</span>
 
         <h2 className="text-neutral-100">{combo.title}</h2>
       </div>
 
-      <div className="flex flex-col gap-3 mt-auto">
+      {/* Цена + кнопка */}
+      <div className="flex items-center justify-between gap-2 mt-auto">
+        {/* Цена */}
         <div className="flex items-center gap-2">
           <span className="text-emerald-400 font-bold text-2xl">
             {combo.price} ₾
@@ -36,6 +41,17 @@ export default function ComboCard({ combo }: ComboCardProps) {
             </span>
           )}
         </div>
+
+        {/* Facebook */}
+        <a
+          href="https://www.facebook.com/profile.php?id=100093207031308"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center gap-2 bg-neutral-800 p-2 rounded-lg cursor-pointer hover:bg-emerald-500 hover:text-neutral-900 transition-colors ease-in-out"
+        >
+          <span>შესაძენად აქ მოგვწერეთ</span>
+          <FaFacebook className="w-6 h-6" />
+        </a>
       </div>
     </li>
   );
